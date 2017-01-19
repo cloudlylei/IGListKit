f322eecaedcbbc8bcad4ea3a0e758f79a7cb880e
Ryan Nystrom <rnystrom@fb.com>
2017-01-19 17:39:54 -0500

    Add adapter tests

--- Tests/IGListBatchUpdateDataTests.m
+++ Tests/IGListBatchUpdateDataTests.m
@@ -171,6 +171,4 @@ static IGListMoveIndex *newMove(NSUInteger from, NSUInteger to) {
     XCTAssertEqualObjects(result.insertSections, indexSet(@[@1]));
 }
 
-// any section that is moved w/ an item-level op in it should: discard all item ops, convert to delete+insert
-
 @end
